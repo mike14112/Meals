f95066e83cf4bd7228b8b0cf1db7cef83538d051
Mike <89590104+mike14112@users.noreply.github.com>
2024-03-10 06:41:46 -0700

    finished the layout, added a modal window, placing an order

--- app.js
+++ app.js
@@ -1,133 +1,110 @@
 //// кнопки  tab
-const   btnTabs = document.querySelectorAll('.btn-nav')
-const tabsContents = document.querySelectorAll('.tab-content')
-
-
+const btnTabs = document.querySelectorAll(".nav-item");
+const tabsContents = document.querySelectorAll(".tab-content");
+//form
+const form = document.querySelector(".form");
+const btnsRadio = document.querySelectorAll(".btn-radio");
+// modal
+const modal = document.querySelector(".modal");
+const btnClose = document.querySelector(".btn-close-modal");
+form.addEventListener("submit", (event) => {
+  event.preventDefault;
+});
 // кнопки корзины
 
+// кнопки добавление
+const btnAdds = document.querySelectorAll(".btn-add");
 
-// кнопки добавление 
-const btnAdds = document.querySelectorAll('.btn-add')
-
-
-//элэменты корзины  
-const  cartHeader = document.querySelector('.cart-header-empty'),
-       mainCart = document.querySelector('.main-cart'),
-       listsItems = document.querySelector('.lists-item'),
-       itemCount = document.querySelector('.item-count'),
-       priceCount = document.querySelector('.price-count')
-       const btnBasket = document.querySelector('.cart-btn'),
-       btnHIde = document.querySelector('.btn-hide')
-/// продукты 
-const cards = document.querySelectorAll('.card')
-
+//элэменты корзины
+const cartHeader = document.querySelector(".cart-header-empty"),
+  mainCart = document.querySelector(".main-cart"),
+  listsItems = document.querySelector(".lists-item"),
+  itemCount = document.querySelector(".item-count"),
+  priceCount = document.querySelector(".price-count");
+const btnBasket = document.querySelector(".cart-btn"),
+  btnHIde = document.querySelector(".btn-hide");
+const btnCheckout = document.querySelector(".btn-checkout");
+/// продукты
+const cards = document.querySelectorAll(".card");
 
 // onclick(btnTabs[i].style.backgroundColor = 'black')
 
+/////////// табы
 
-/////////// табы 
+btnTabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    let tabId = tab.getAttribute("data-tab");
 
+    if (tabId != null) {
+      tabsContents.forEach((content) => {
+        content.classList.remove("active");
+        btnTabs.forEach((btn) => {
+          btn.style.backgroundColor = "#FFFFFF";
+        });
+      });
+      document.getElementById(tabId).classList.add("active");
 
-btnTabs.forEach( tab => {
-    tab.addEventListener('click', () => {
-        let  tabId = tab.getAttribute('data-tab')
+      tab.style.backgroundColor = "   #FFAB08";
+    } else {
+      console.log("dsdad");
+    }
+  });
+});
 
-        if(tabId != null){
-            tabsContents.forEach( content => {
-                content.classList.remove('active')
-                btnTabs.forEach(btn => {
-                btn.style.backgroundColor = '#FFFFFF'
-               })
-              
-            })
-            document.getElementById(tabId).classList.add('active')
-               
-            tab.style.backgroundColor = '   #FFAB08'
-        }else{
-            console.log('dsdad')
-        }
+const products = [];
 
-
-    
-        })
-        
-})
-
-
-
-
-
-
-
-const products = []
-
-
-
-
-
-//addTocart 
+//addTocart
 
 const addTocart = (obj) => {
+  products.push(obj);
 
-
-products.push(obj)
-
-renderCart(products)
-
-
-}
-
-
+  renderCart(products);
+};
 
 //  //показ-скрытие корзины   с помощью кнопки
 
-btnHIde.addEventListener('click' ,() => {
-  mainCart.classList.toggle('hide')
-})
-btnBasket.addEventListener('click', () => {
-  mainCart.classList.toggle('hide')
+btnHIde.addEventListener("click", () => {
+  mainCart.classList.toggle("hide");
+});
+btnBasket.addEventListener("click", () => {
+  mainCart.classList.toggle("hide");
+});
 
-})
+//показ-скрытие корзины
 
-  //показ-скрытие корзины
-
-const hideMainCart =  (arr) => {
-  if(arr.length == 0){
-    mainCart.classList.add('hide')
-    cartHeader.textContent = ' В корзине пока пусто :( '
-
+const hideMainCart = (arr) => {
+  if (arr.length == 0) {
+    mainCart.classList.add("hide");
+    cartHeader.textContent = " В корзине пока пусто :( ";
   }
-    if(arr.length > 0){
-      mainCart.classList.remove('hide')
-      cartHeader.textContent = ''
-
-    }
-  
-}
-
-
-
+  if (arr.length > 0) {
+    mainCart.classList.remove("hide");
+    cartHeader.textContent = "";
+  }
+};
 
 const createCartObject = (btns, cards) => {
-let product = {}
+  let product = {};
 
-btns.forEach((btn, idx) => {
-    btn.setAttribute('data-index', `${idx}`)
+  btns.forEach((btn, idx) => {
+    btn.setAttribute("data-index", `${idx}`);
 
-  btn.addEventListener('click', () => {
-    const item = cards[idx]
-      const img = item.querySelector('.img').getAttribute('src');
-  const name = item.querySelector('.name').textContent;
-  const price = parseFloat(item.querySelector('.price').textContent);
-  const weight = parseFloat(item.querySelector('.weight').textContent);
+    btn.addEventListener("click", () => {
+      const item = cards[idx];
+      const img = item.querySelector(".img").getAttribute("src");
+      const name = item.querySelector(".name").textContent;
+      const price = parseFloat(item.querySelector(".price").textContent);
+      const weight = parseFloat(item.querySelector(".weight").textContent);
 
-
-
-      const existingItem = products.find(cartItem => cartItem.name === name && cartItem.price === price && cartItem.weight === weight );
+      const existingItem = products.find(
+        (cartItem) =>
+          cartItem.name === name &&
+          cartItem.price === price &&
+          cartItem.weight === weight
+      );
 
       if (existingItem) {
-       alert(`Этот  Товар Уже Есть ${name} В корзине `)
-
+        alert(`Этот  Товар Уже Есть ${name} В корзине `);
       } else {
         product.img = img;
         product.name = name;
@@ -135,24 +112,20 @@ btns.forEach((btn, idx) => {
         product.weight = weight;
         product.count = 1;
         addTocart(product);
-        product = {}
-
+        product = {};
       }
-  })
-
-})
-}
-
-
-
-createCartObject(btnAdds, cards)
+    });
+  });
+};
+let totalPriceItems;
+createCartObject(btnAdds, cards);
 
 //itemsCart & control
 
-const renderCart =  (products) => {
-  listsItems.innerHTML = ''
+const renderCart = (products) => {
+  listsItems.innerHTML = "";
   products.forEach((item, idx) => {
-    listsItems.innerHTML +=`
+    listsItems.innerHTML += `
     <li> 
     <img src="img/items/1-1.png" alt="" style="width: 64px;" height="52px"> 
     <div class="item-descr">
@@ -165,50 +138,116 @@ const renderCart =  (products) => {
       <span class="item-count">${item.count}</span>
       <button class="btn-plus" data-index="${idx}">+</button>
     </div>
-  </li>`
- const totalPriceItems = products.reduce((acc, items) => acc += items.price * items.count, 0)
- 
- priceCount.innerHTML = `${totalPriceItems} ₽`
+  </li>`;
+    totalPriceItems = products.reduce(
+      (acc, items) => (acc += items.price * items.count),
+      0
+    );
 
-})
+    priceCount.innerHTML = `${totalPriceItems} ₽`;
+  });
 
-   itemCount.innerHTML = `${products.length}`
-   const btnMinus = document.querySelectorAll('.btn-minus'),
-   btnPlus = document.querySelectorAll('.btn-plus')   
+  itemCount.innerHTML = `${products.length}`;
+  const btnMinus = document.querySelectorAll(".btn-minus"),
+    btnPlus = document.querySelectorAll(".btn-plus");
 
+  hideMainCart(products);
 
-   hideMainCart(products)
-
-
- 
-   controlCart(products, btnMinus, btnPlus)
-}
-
-
+  controlCart(products, btnMinus, btnPlus);
+};
 
 // control Cart
-const  controlCart = ( arr, btnMinus, btnBtnPlus) => {
-
+const controlCart = (arr, btnMinus, btnBtnPlus) => {
   btnMinus.forEach((btn, i) => {
-    if(arr[i].count == 0){
-      arr.splice(i, 1)
-    
-      renderCart(arr)
+    if (arr[i].count == 0) {
+      arr.splice(i, 1);
 
+      renderCart(arr);
     }
-   btn.addEventListener('click', () => {
-   arr[i].count--
-  renderCart(arr)
-   })
-  })
+    btn.addEventListener("click", () => {
+      arr[i].count--;
+      renderCart(arr);
+    });
+  });
   btnBtnPlus.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-        arr[i].count++   
-        renderCart(arr)
-       
-    })
-  })
+    btn.addEventListener("click", () => {
+      arr[i].count++;
+      renderCart(arr);
+    });
+  });
+};
 
+/////Отображение  и скрытие   Modal
 
-}
+btnCheckout.addEventListener("click", () => {
+  modal.classList.remove("hide");
+});
 
+btnClose.addEventListener("click", () => {
+  modal.classList.add("hide");
+});
+
+let isDelivery = false;
+
+btnsRadio.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.value == "delivery") {
+      document.querySelector(".delivery-section").classList.remove("hide");
+      isDelivery = true;
+    } else if (btn.value == "pickup") {
+      document.querySelector(".delivery-section").classList.add("hide");
+      isDelivery = false;
+    }
+  });
+});
+
+// оформление заказа
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputName = document.querySelector(".input-name"),
+    inputPhone = document.querySelector(".input-phone"),
+    inputAdress = document.querySelector(".input-adress"),
+    inputFloor = document.querySelector(".input-floor"),
+    inputIntercom = document.querySelector(".input-intercom");
+  const order = {};
+
+  if (
+    !isDelivery &&
+    inputName.value.length >= 4 &&
+    inputPhone.value.length >= 8
+  ) {
+    order.id = Math.trunc(Math.random() * 950);
+    (order.name = inputName.value), (order.phone = inputPhone.value);
+    order.isDeliver = isDelivery;
+    order.orderItems = products;
+    order.totalPice = totalPriceItems;
+    inputName.value = "";
+    inputPhone.value = "";
+    console.log(order);
+  }
+
+  if (
+    isDelivery &&
+    inputName.value.length >= 4 &&
+    inputPhone.value.length >= 8 &&
+    inputAdress.value.length >= 4 &&
+    inputFloor.value.length >= 1 &&
+    inputIntercom.value.length >= 2
+  ) {
+    order.name = inputName.value;
+    order.phone = inputPhone.value;
+    order.adress = inputAdress.value;
+    order.floor = inputFloor.value;
+    order.interCom = inputIntercom.value;
+    order.isDeliver = isDelivery;
+    order.orderItems = products;
+    order.totalPice = totalPriceItems;
+
+    inputName.value = "";
+    inputPhone.value = "";
+    inputAdress.value = "";
+    inputFloor.value = "";
+    inputIntercom.value = "";
+    console.log(order);
+  }
+});
