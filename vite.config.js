2d4b0c04808d8aa8e5109d2822f5bc87e20adc7e
mike <mixailted@gmail.com>
2024-07-14 13:27:58 +0400

    change: name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base: '/https://mike14112.github.io/Meals/',
+  base: '/meals',
   plugins: [
     vue(),
   ],
