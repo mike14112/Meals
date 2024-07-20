c2241de7cb513a4169d98bbd24a6c34799001e29
mike <mixailted@gmail.com>
2024-07-20 16:45:50 +0400

    Fix:  modal markup

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -54,7 +54,7 @@ const createOrder = () => {
     <div class="modal bg-black  w-full h-full z-20 fixed left-2/4 top-2/4 translate-x-[-50%] translate-y-[-50%]  sm:top-[40%] "
         v-if="showIsModal">
         <div
-            class="modal-window relative flex w-[42.8rem] h-[29rem] bg-white mt-24 mx-auto rounded-3xl sm:w-full sm:h-full sm:py-8 sm:px-2.5 sm:flex sm:flex-col sm:items-center  ">
+            class="modal-window relative flex w-[42.8rem] h-[29rem] bg-white mt-24 mx-auto rounded-3xl sm:w-full sm:h-full sm:py-8 sm:px-2.5 sm:flex sm:flex-col sm:items-center   ">
             <div class="side-modal bg-[#ffab08] rounded-t-3xl rounded-l-3xl py-16 px-6 md:hidden sm:hidden">
                 <img src="/img/modal-img.png" alt="">
             </div>
@@ -65,15 +65,16 @@ const createOrder = () => {
                 <form action=""
                     class="form h-full flex flex-col  justify-around pt-4 gap-2 sm:justify-between sm:py-8 sm:px-2.5 "
                     @submit.prevent="createOrder">
-                    <div class="modal-group flex flex-col gap-2 ">
+                    <div class="modal-group flex flex-col gap-4 p-1  ">
                         <input type="text" placeholder="Ваше Имя" minlength="4" maxlength="10"
-                            class="input-name mt-3 border-none rounded-xl text-[#b1b1b1] " v-model.trim="formData.name">
-                        <input type="number" minlength="12" maxlength="12" placeholder="Телефон"
-                            class="input-phone mt-3 border-none rounded-xl text-[#b1b1b1]"
+                            class="input-name mt-3 border-none rounded-xl text-[#b1b1b1] w-72  p-1"
+                            v-model.trim="formData.name">
+                        <input type="number" minlength="12" maxlength="12" placeholder="Телефон  "
+                            class="input-phone mt-3 border-none rounded-xl text-[#b1b1b1] w-72 p-1"
                             v-model.trim="formData.phone">
-                        <div class="radio-section flex flex-col ">
+                        <div class="radio-section flex flex-col gap-4 ">
                             <span>
-                                <input type="radio" value="pickup" checked name="btn-radio" class="btn-radio"
+                                <input type="radio" value="pickup" checked name="btn-radio" class="btn-radio "
                                     v-model="formData.isTake">
                                 <label for="pickup">Самовывоз</label>
                             </span>
@@ -84,13 +85,13 @@ const createOrder = () => {
                             </span>
                         </div>
                         <div class="delivery-section  w-[18.5rem] sm:mb-[22rem]" v-show="formData.isTake == 'delivery'">
-                            <input type=" text" placeholder="Улица, дом, квартира" class="input-adress"
-                                v-model="formData.adress">
-                            <div class="section-home-input flex gap-5">
-                                <input type="number" class="input-floor w-36" min="1" max="100" maxlength="2"
-                                    placeholder="Этаж" v-model="formData.floor">
-                                <input type="number" placeholder="Домофон" maxlength="4" min="1" max="999"
-                                    class="input-intercom w-36" v-model="formData.cityCom">
+                            <input type=" text" placeholder="Улица, дом, квартира"
+                                class="input-adress mb-4 rounded-xl p-1" v-model="formData.adress">
+                            <div class="section-home-input flex gap-12">
+                                <input type="number" class="input-floor w-36 rounded-xl p-1" min="1" max="100"
+                                    maxlength="2" placeholder="Этаж" v-model="formData.floor">
+                                <input type="number " placeholder="Домофон" maxlength="4" min="1" max="999"
+                                    class="input-intercom w-36 rounded-xl p-1" v-model="formData.cityCom">
                             </div>
                         </div>
                     </div>
